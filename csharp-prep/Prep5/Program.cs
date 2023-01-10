70257a8dead4a9052aa1119e1d2a1260ad410cca
Joel Hix <jdan74@gmail.com>
2023-01-09 16:34:45 -0800

    finished functions exercise

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        
+    static void DisplayWelcome ()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    } 
+    
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter favorite number: ");
+        string userResponse = Console.ReadLine();
+        int favNum = int.Parse(userResponse);
+        return favNum;
+    }
+    static int SquareNumber(int favNum)
+    {
+        int square = favNum * favNum;
+        return square;
+    }
+    static void DisplayResult(string name, int square)
+    {
+        Console.Write($"{name}, the square of your number is {square} ");
+
+    }
+
+    DisplayWelcome ();  //Welcome to the program!
+        string name = PromptUserName(); //Please enter your name: Brother Burton
+        int favNum = PromptUserNumber();//Please enter your favorite number: 42
+        int square = SquareNumber(favNum);//sqare the number
+        DisplayResult(name, square);
+
+
+    }
+
+    private static void PromptUserName()
+    {
+        throw new NotImplementedException();
     }
 }
