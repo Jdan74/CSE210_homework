01104f4c5a117ae0de7b69aaabc743bd02b9a737
Joel Hix <jdan74@gmail.com>
2023-01-28 22:48:24 -0800

    finished program

---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,37 @@
+public class Fraction
+{
+
+//member variables
+private int _top;
+private int _bottom;
+
+
+
+// constructors
+public Fraction()
+{
+}
+
+
+public Fraction(int wholeNumber)
+{
+
+}
+public Fraction(int top, int bottom)
+{
+
+}
+
+//getters and setters
+public int GetNumerator()
+{
+    return _top;
+}
+public int GetDenomintor()
+{
+    return _bottom;
+}
+
+
+
+}
