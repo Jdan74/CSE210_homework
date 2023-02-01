ff2b2d363b5d09c810602a1ffc7bf935fd6f6710
Joel Hix <jdan74@gmail.com>
2023-02-01 12:51:21 -0800

    finished fraction class and program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        
+        Fraction Fraction1 = new Fraction();//calling the generic constructor
+        
+        Fraction1.SetTop(1);//using the setter on this instance to set top value to 3
+        Fraction1.SetBottom(1);//using the setter on this instance to set bottom value to 4
+              
+        Fraction Fraction2 = new Fraction(5);//calling the constructor that accepts one whole number and set it to the top number and sets 1 as the bottom
+
+        Fraction Fraction3 = new Fraction(3,4);//calling the construction that accepts a 2 numbers, a top and then bottom
+
+        Fraction Fraction4 = new Fraction(1,3);//calling the construction that accepts a 2 numbers, a top and then bottom
+
+        
+        
+        
+        Console.WriteLine("displaying Fraction1");
+        Fraction1.GetFractionString();//below we are calling both of our methods for each of these instances
+        Fraction1.GetDecimalValue();
+        
+        Console.WriteLine("displaying Fraction2");
+        Fraction2.GetFractionString();
+        Fraction2.GetDecimalValue();
+        
+        Console.WriteLine("displaying Fraction3");
+        Fraction3.GetFractionString();
+        Fraction3.GetDecimalValue();
+        
+        Console.WriteLine("displaying Fraction4");
+        Fraction4.GetFractionString();
+        Fraction4.GetDecimalValue();
+
+
+
+
     }
 }
--- prepare/Learning03/fraction.cs
+++ prepare/Learning03/fraction.cs
@@ -7,31 +7,71 @@ private int _bottom;
 
 
 
-// constructors
+// constructors//remember that return types are left EMPTY on constructors
 public Fraction()
 {
 }
 
 
-public Fraction(int wholeNumber)
+public Fraction(int wholeNumber)// requires a whole number
 {
 
+   _top = wholeNumber;
+   _bottom = 1;
+
 }
-public Fraction(int top, int bottom)
+public Fraction(int top, int bottom)//requires a numerator and a denominator
 {
 
+    _top = top;
+    _bottom = bottom;
+
 }
 
-//getters and setters
-public int GetNumerator()
+//getters and setters = special methods specific to a class to allow other classes to set and get 
+//the variable values without actually seeing or working direction with the class member variables.
+//THESE need return types, just like class methods.
+public int GetTop()//public so accessible, returns an int of the value of _top variable
 {
     return _top;
 }
-public int GetDenomintor()
+public void SetTop(int top)//public so accessible, void as returns nothong, accepts one int and sets the value of the _top variable, 
+{
+    _top =  top;
+}
+public int GetBottom()//public so accessible, returns an int from the _bottom variable
 {
     return _bottom;
 }
 
+public void SetBottom(int bottom)//public so it's accessible, returns nothing, accepts one int called bottom and sets it to _bottom
+{
+ _bottom = bottom;
+}
+
+
+
+//methods
+
+public void GetFractionString()
+{
+
+Console.WriteLine($"{_top}/{_bottom}");
+
+}
+
+public void GetDecimalValue()// (https://stackoverflow.com/questions/1043164/why-does-decimal-divideint-int-work-but-not-int-int - int is an integer type; dividing two ints performs an 
+//integer division, i.e. the fractional part is truncated since it can't be stored in the result type 
+//(also int!). Decimal, by contrast, has got a fractional part. By invoking Decimal.Divide, your int arguments
+// get implicitly converted to Decimals. You can enforce non-integer division on int arguments by explicitly 
+//casting at least one of the arguments to a floating-point type, e.g.:)
+
+{
+    double result = (double) _top / _bottom;
+    Console.WriteLine(result);
+    
+    //Console.WriteLine(Decimal.Divide(_top,_bottom)); This method also works
+}
 
 
 }
