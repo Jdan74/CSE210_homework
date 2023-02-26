ebe5d936e60c23412143816fbf8764f3d1757b9e
Joel Hix <jdan74@gmail.com>
2023-02-25 16:55:17 -0800

    added more logic to limit inputs on breathing

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -14,11 +14,31 @@ public BreathingActivity(string activityName, string activityInstruction) : base
 //Methods_________________________________________________________________________________________
 private void DisplayActivityPrompt()// 
 {   
-    Console.WriteLine("How many seconds would you like your inhale or exhale to last? ");
-    string breathTime = Console.ReadLine();
-    int t = Int32.Parse(breathTime);//have to parse this to an int
-    Console.WriteLine();
-    _remainingSeconds = _seconds;
+int t = 0;//a variable to hold the time int
+string breathTime ="";//a variable to hold the breathTime we'll get from the console
+bool isNumber = false;//a bool variable to use in our loop below to check if our input is a number
+_remainingSeconds = _seconds;//a variable to hold our original time variable for use with our stopwatch
+
+while (!isNumber)
+{
+    Console.WriteLine("How many seconds would you like the breathing interval to be? Input a number between 2-6");
+    breathTime = Console.ReadLine();
+    if(int.TryParse(breathTime, out t))//this will try to parse the string to an int and if it can it will set to variable t and run the first statment, otherwise return false and run the second
+    {
+        isNumber = true;//setting our bool to true so we can exit the loop
+        if (t < 2 || t > 6 )// but first, lets check if the int is within the range we desired
+        {
+            Console.WriteLine("You have made an unacceptable entry.  Please try again...");//error message
+            isNumber = false;//set to false so we stay in the loop
+        }
+    }
+    else
+    {
+    Console.WriteLine("You have made an unacceptable entry.  Please try again...");//error message and bool stays false
+    }
+
+}
+   
     GetReady(4);
     Stopwatch s = new Stopwatch();
     s.Start();
@@ -48,7 +68,7 @@ private void DisplayActivityPrompt()//
 
 public void RunBreathingActivity()
 {
-    Console.BackgroundColor = ConsoleColor.Blue;
+    Console.BackgroundColor = ConsoleColor.Blue;//just for fun, let's chage the color
     Console.Clear();
     DisplayActivityStartup();//display the startup messages
     DisplayActivityPrompt();
