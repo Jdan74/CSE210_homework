1ca9a9e7d609487c142c5c0926415f784ee2d364
Joel Hix <jdan74@gmail.com>
2023-03-01 20:54:17 -0800

    Completed Polymorphism Learning 05 shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+       
+       List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(new Square("blue", 5));
+        shapeList.Add(new Rectangle("red", 10, 3));
+        shapeList.Add(new Circle("yellow", 3));
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine($"the color of the {shape.GetType()} is {shape.GetColor()} and the area is {shape.GetArea()}");
+        }  //used the .GetType() method to show me the child class name as well....sweet.  This is so fun!
     }
 }
